5e135448d7ab1dd70b6222e6f80def88d0a6ba53
champer3 <okunolas21@gmail.com>
2022-07-15 12:48:11 +0100

    complete landing page using tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+  },
+  plugins: [],
+};
