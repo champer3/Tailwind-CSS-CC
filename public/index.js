1f46161106f3b716245d2f3642ed8a16bb9a766d
champer3 <okunolas21@gmail.com>
2022-07-24 15:58:35 +0100

    Coding Challenges

--- public/index.js
+++ public/index.js
@@ -102,4 +102,144 @@ var runningSum = function (nums) {
 
 // console.log(runningSum([1, 2, 3, 4, 5]));
 
-function anagram(str1, str2) {}
+function anagram(str1, str2) {
+  str1 = str1.toLowerCase().replace(/\s+/g, "");
+  str2 = str2.toLowerCase().replace(/\s+/g, "");
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  let strObj1 = {};
+  let strObj2 = {};
+  for (let el of str1) {
+    strObj1[el] = (strObj1[el] || 0) + 1;
+  }
+  for (let el of str2) {
+    strObj2[el] = (strObj2[el] || 0) + 1;
+  }
+  for (let val in strObj1) {
+    if (!(val in strObj2)) return false;
+    if (strObj1[val] !== strObj2[val]) return false;
+  }
+
+  return true;
+}
+
+// console.log(anagram("zazu", "uzaz"));
+
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) return [arr[i], arr[j]];
+    }
+  }
+}
+
+// console.log(sumZero([-3, -2, -1, 0, 1, 2, 3, 4]));
+
+function sumZero1(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+// console.log(sumZero1([-3, -2, -1, 0, 1, 2, 3, 4]));
+
+// Write a function called countUniqueValues, which accepts an Array and counts the number of unique values in the Array
+
+//Refactored Solution - Big O of n 0(n) solution
+
+function countUniqueValues(arr) {
+  if (arr.length === 0) return 0;
+  let left = 0;
+  let right = 1;
+  while (right < arr.length) {
+    if (arr[left] !== arr[right]) {
+      left++;
+      arr[left] = arr[right];
+    }
+    right++;
+  }
+  return left + 1;
+}
+
+// console.log(
+//   countUniqueValues([1, 1, 1, 1, 1, 2, 3, 3, 3, 4, 5, 5, 6, 6, 7, 8])
+// );
+
+//Write a function which accepts an array of integers and a number called n.
+// The function should calculate the maximum sum of n consecutive elements in the array
+// Big O of n^2 0(n^2) solution
+
+function maxSubarraySum(arr, n) {
+  if (n > arr.length) return null;
+  let max = -Infinity;
+  for (let i = 0; i < arr.length - n + 1; i++) {
+    let temp = 0;
+    for (let j = 0; j < n; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
+
+// console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+
+//Write a function which accepts an array of integers and a number called n.
+// The function should calculate the maximum sum of n consecutive elements in the array
+//Refactored Solution - Big O of n 0(n) solution
+
+function maxSubarraySum1(arr, n) {
+  if (n > arr.length) return null;
+  let tempSum = 0;
+  let maxSum = 0;
+  for (let i = 0; i < n; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = n; i < arr.length; i++) {
+    tempSum = tempSum + arr[i] - arr[i - n];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+// console.log(maxSubarraySum1([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+
+function sameFrequency(n, m) {
+  // good luck. Add any arguments you deem necessary.
+  let nText = n.toString();
+  let mText = m.toString();
+  if (nText.length !== mText.length) return false;
+  let nObj = {};
+  let mObj = {};
+  for (let i = 0; i < nText.length; i++) {
+    nObj[nText[i]] = nObj[nText[i]] + 1 || 1;
+  }
+  for (let i = 0; i < mText.length; i++) {
+    mObj[mText[i]] = mObj[mText[i]] + 1 || 1;
+  }
+  for (let key in nObj) {
+    if (!(key in mObj)) {
+      return false;
+    }
+    if (nObj[key] !== mObj[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(sameFrequency(122, 132));
+
